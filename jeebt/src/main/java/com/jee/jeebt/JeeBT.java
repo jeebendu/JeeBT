43f98478f444ec99c914e32dc3e2278ddc1b942d
jeeebndu <jeeebndu2010@gmail.com>
2021-07-27 03:49:41 +0530

    bluetooth added

--- jeebt/src/main/java/com/jee/jeebt/JeeBT.java
+++ jeebt/src/main/java/com/jee/jeebt/JeeBT.java
@@ -1,6 +1,16 @@
 package com.jee.jeebt;
 
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+
+import java.util.Set;
+
 public class JeeBT {
+
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
 
     public String version (){
         return "v1.0.0";
@@ -8,5 +18,16 @@ public class JeeBT {
 
     public String info (){
         return "Jee Bluetooth App";
+    }
+
+    public Intent getBluetooth(){
+        BA = BluetoothAdapter.getDefaultAdapter();
+        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        return turnOn;
+    }
+
+    public Set<BluetoothDevice> getPairedDevices (){
+        pairedDevices = BA.getBondedDevices();
+        return pairedDevices;
     }
 }
